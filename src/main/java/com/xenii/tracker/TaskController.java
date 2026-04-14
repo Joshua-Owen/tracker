75b0b1a02e1612ec2f48e348fdc71401a5d6958e
Xenii <joshua068@hotmail.co.uk>
2026-04-14 16:10:34 +0100

    delete and post features added

--- src/main/java/com/xenii/tracker/TaskController.java
+++ src/main/java/com/xenii/tracker/TaskController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
@@ -14,14 +15,45 @@ public class TaskController {
   
  
     @GetMapping
-    public List<Task> getTasks() {
+    public List<Task> getTasks() 
+    {
         return tasks;
     }
 
     @PostMapping
-    public Task addTask(@RequestBody Task task) {
-        Task newTask = new Task(nextId++, task.getTitle(), task.getCompleted());
+    public Task addTask(@RequestBody Task task) 
+    {
+        Boolean completed = task.getCompleted() != null ? task.getCompleted() : false;
+        
+        Task newTask = new Task(nextId++, task.getTitle(), completed);
         tasks.add(newTask);
         return newTask;
     }
+
+    @DeleteMapping("/{id}")
+    public String deleteTask(@PathVariable Long id)
+    {
+        tasks.removeIf((task) -> task.getId().equals(id));
+        return "Task deleted";
+    }
+
+    @PutMapping("/{id}")
+    public Task putMethodName(@PathVariable Long id, @RequestBody Task updatedTask) 
+    {
+        
+        for (Task task : tasks)
+        {
+            if (task.getId().equals(id))
+            {
+                Boolean completed = updatedTask.getCompleted() != null ? updatedTask.getCompleted() : false;
+
+                task.setTitle(updatedTask.getTitle() != null ? updatedTask.getTitle() : task.getTitle());
+                task.setCompleted(completed);
+                return task;
+            }
+        }
+        throw new RuntimeException("Task not found");
+    }
+    
+    
 }
